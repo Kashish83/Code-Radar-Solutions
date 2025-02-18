e9dbf33a473c43eaebfb48d38dd56ec5c3522f14
Kashish83 <kashishwadhwa65007@gmail.com>
2025-02-18 10:37:09 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 int main(){
     char ch;
-    scanf("%c",&ch){
+    scanf("%c",&ch);
         if(isdigit(ch)){
             printf("Digit\n");
         }else if(isalpha(ch)){
@@ -17,4 +17,3 @@ int main(){
                 printf("Special Character\n");
             }
     }
-}
